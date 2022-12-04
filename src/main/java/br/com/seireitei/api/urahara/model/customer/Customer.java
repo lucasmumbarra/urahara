437e15626b644f9bdf99c330e847dabd001d3db2
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-12-04 16:01:56 -0300

    Added exceptions

--- src/main/java/br/com/seireitei/api/urahara/model/customer/Customer.java
+++ src/main/java/br/com/seireitei/api/urahara/model/customer/Customer.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.apache.logging.log4j.util.Strings;
 import org.hibernate.annotations.GenericGenerator;
@@ -16,19 +17,22 @@ import lombok.Data;
 @Entity
 @Table(name = "customer")
 public class Customer {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO, generator = "increment")
   @GenericGenerator(name = "increment", strategy = "increment")
   @Column(nullable = false, name = "ID")
   private Integer id;
 
-  @Column(nullable = false, name ="uuid")
+  @Column(nullable = false, name = "uuid")
   private String uuid;
 
+  @NotEmpty(message = "O campo nome é obrigatório")
   @Column(nullable = false, length = 150)
   private String name;
 
+  @NotNull(message = "O CPF/CNPJ é obrigatório")
+  @Size(min = 11, max = 14, message = "O CPF/CNPJ deve conter de 11 a 14 caracteres")
   @Column(nullable = false, length = 14)
   private String document;
 
