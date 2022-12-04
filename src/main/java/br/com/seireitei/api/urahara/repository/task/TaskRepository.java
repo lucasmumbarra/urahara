b8d96fc5307c879b95a1fdfa2e327368ca5b39bc
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-12-04 16:45:51 -0300

    refactored code

--- src/main/java/br/com/seireitei/api/urahara/repository/task/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.seireitei.api.urahara.repository.task;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.seireitei.api.urahara.model.task.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-  
-}
