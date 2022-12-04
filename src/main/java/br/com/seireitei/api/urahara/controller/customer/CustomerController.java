671a1b71326776a5ef66a01a327b3c4812dee20a
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-12-04 14:46:29 -0300

    Added route get customer by id

--- src/main/java/br/com/seireitei/api/urahara/controller/customer/CustomerController.java
+++ src/main/java/br/com/seireitei/api/urahara/controller/customer/CustomerController.java
@@ -4,6 +4,7 @@ import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.seireitei.api.urahara.model.customer.Customer;
 import br.com.seireitei.api.urahara.repository.customer.CustomerRepository;
@@ -17,8 +18,12 @@ public class CustomerController {
 
   @PostMapping
   @ResponseStatus(CREATED)
-  public Customer save( @RequestBody Customer customer) {
+  public Customer save(@RequestBody Customer customer) {
     return repository.save(customer);
   }
-  
+
+  @GetMapping(path = "{id}")
+  public Customer findByCustomerId(@PathVariable Integer id) {
+    return repository.findById(id).orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+  }
 }
--- src/main/java/br/com/seireitei/api/urahara/repository/task/TaskRepository.java
+++ src/main/java/br/com/seireitei/api/urahara/repository/task/TaskRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.seireitei.api.urahara.model.task.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Integer> {
   
 }
