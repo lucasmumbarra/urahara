a9d9a8028a87a00e77f7088b2e859cf8b5690ad4
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-12-09 20:10:45 -0300

    Added Date model and Time zones

---
+++ src/main/java/br/com/seireitei/api/urahara/model/DateModel.java
@@ -0,0 +1,57 @@
+package br.com.seireitei.api.urahara.model;
+
+import static br.com.seireitei.api.urahara.utils.DatePatterns.YYY_MM_DD_T_HH_MM_SS;
+import static br.com.seireitei.api.urahara.utils.Dates.getFormattedDate;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@Embeddable
+public class DateModel implements Serializable {
+  
+  private static final long serialVersionUID = 7191366391960359555L;
+
+  public static final String CREATED_AT = "createdAt";
+  public static final String UPDATED_AT = "updatedAt";
+  public static final String DELETED_AT = "deletedAt";
+
+  @Column(nullable = false, name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(nullable = false, name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @Column(nullable = true, name = "created_at")
+  private LocalDateTime deletedAt;
+
+  public DateModel() {
+  }
+  
+  public DateModel(LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+
+  public DateModel(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public String formattedCreatedAt() {
+    return getFormattedDate(createdAt, YYY_MM_DD_T_HH_MM_SS);
+  }
+
+  public String formattedUpdatedAt() {
+    return getFormattedDate(updatedAt, YYY_MM_DD_T_HH_MM_SS);
+  }
+}
---
+++ src/main/java/br/com/seireitei/api/urahara/model/TimeZones.java
@@ -0,0 +1,26 @@
+package br.com.seireitei.api.urahara.model;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+public class TimeZones {
+  
+  private static final String AMERICA_SAO_PAULO = "America/Sao_Paulo";
+  private static final String UTC = "UTC";
+
+  public static void setDefault() {
+    TimeZone.setDefault(TimeZone.getTimeZone(AMERICA_SAO_PAULO));
+  }
+
+  public static void setUTC() {
+    TimeZone.setDefault(TimeZone.getTimeZone(UTC));
+  }
+
+  public static ZoneId getDefault() {
+    return ZoneId.of(AMERICA_SAO_PAULO);
+  }
+
+  public static ZoneId getUtc() {
+    return ZoneId.of(UTC);
+  }
+}
